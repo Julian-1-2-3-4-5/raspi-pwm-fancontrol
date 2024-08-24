3fdcddd6a6fa52475f44626cb71afeee68a24fb8
Julian Stauffer <julian.stauffer.js@gmail.com>
2024-08-24 22:57:52 +0200

    add License file + boilerplate notices to all scripts (GPLv3+)

--- idea-tests/fancontrol.py
+++ idea-tests/fancontrol.py
@@ -1,3 +1,21 @@
+#    A Script to control a pwm fan via pigpiod
+#
+#    Copyright (C) 2024 Julian Stauffer julian.stauffer.js@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
 #install and run "sudo pigpiod" before strting this script
 import pigpio #library to use GPIO pins (only one that works with hardware pwm)
 import time
--- idea-tests/tempcurve.py
+++ idea-tests/tempcurve.py
@@ -1,3 +1,21 @@
+#    A Script to get a fan speed pwm value for a sepecified temperature according to a temp/fan-curve
+#
+#    Copyright (C) 2024 Julian Stauffer julian.stauffer.js@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
 #temp curve points
 #temp[x]=Temperature at point; p[x]=PWM-Duty(PWM percent at point)
 temp1=25
